febe356cf7e6b4ae88853446146071f084405ae9
s1-dev <sean49naughton@gmail.com>
2024-06-26 00:47:35 -0400

    Revert changes on this file, didn't mean to commit

--- src/bot/tempIrcClient.py
+++ src/bot/tempIrcClient.py
@@ -1,59 +1,54 @@
 import socket
-import threading
+import ssl
 
-class IRCClient:
-    def __init__(self, server, port, isMaster):
+class SimpleIRCClient:
+    def __init__(self, server, port, nickname, username, realname, use_ssl=False):
         self.server = server
         self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        if isMaster == "MASTER":
-            self.nickname = "MASTER_NICK"
-            self.username = "MASTER_UNAME"
-            self.realname = "MASTER_RNAME"
-            self.passphrase = "test"
-            self.identifier = "MASTER"
-        else:
-            self.nickname = "BOT_NICK"
-            self.username = "BOT_UNAME"
-            self.realname = "BOT_RNAME"
-            self.passphrase = "test123"
-            self.identifier = "BOT"
-        self.channel = "#testChannel"
-    
+        self.nickname = nickname
+        self.username = username
+        self.realname = realname
+        self.use_ssl = use_ssl
+        self.sock = None
+
     def connect(self):
+        raw_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if self.use_ssl:
+            self.sock = ssl.wrap_socket(raw_socket)
+        else:
+            self.sock = raw_socket
+
         self.sock.connect((self.server, self.port))
-        self.send_command(f"/VALIDATE {self.passphrase} {self.identifier}")
-        self.send_command(f"/NICK {self.nickname}")
-        self.send_command(f"/USER {self.username} 0 * :{self.realname}")
-        self.send_command(f"/JOIN {self.channel}")
-        
-        # Start a thread to receive messages
-        threading.Thread(target=self.receive_messages, daemon=True).start()
-        
-        print(f"Connected to {self.server} on port {self.port} as {self.nickname}")
-    
+        #self.send_command(f"NICK {self.nickname}")
+        #self.send_command(f"USER {self.username} 0 * :{self.realname}")
+
     def send_command(self, command):
-        self.sock.sendall(f"{command}\r\n".encode('utf-8'))
-    
-    def receive_messages(self):
-        while True:
-            response = self.sock.recv(4096).decode('utf-8')
-            if response.startswith("PING"):
-                self.send_command(f"PONG {response.split()[1]}")
-            else:
-                print(response)
-    
-    def start(self):
-        self.connect()
-        while True:
-            command = input("Enter command: ")
-            self.send_command(command)
+        self.sock.sendall((command + "\r\n").encode('utf-8'))
+
+    def receive(self):
+        response = self.sock.recv(4096).decode('utf-8')
+        return response
+
+    def run(self):
+        try:
+            while True:
+                test123 = input("Input: ")
+                self.send_command(test123)
+                response = self.receive()
+                print(response.strip())
+                if "PING" in response:
+                    self.send_command("PONG " + response.split()[1])
+        except KeyboardInterrupt:
+            self.send_command("QUIT")
+            self.sock.close()
 
 if __name__ == "__main__":
     server = "127.0.0.1"
     port = 6667  # Use 6667 for non-SSL, 6697 for SSL
+    nickname = "testnick"
+    username = "testuser"
+    realname = "Test User"
 
-    botOrMaster = input("bot or master: ")
-
-    client = IRCClient(server, port, botOrMaster)
-    client.start()
+    client = SimpleIRCClient(server, port, nickname, username, realname)
+    client.connect()
+    client.run()
